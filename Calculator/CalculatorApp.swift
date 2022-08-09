a518e947d53f6507e9248dc0a64bf5bd67f660e1
Rodz Amor <ramorSTEM2022@gmail.com>
2022-08-09 17:10:05 -0400

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Andrie on 8/9/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
